b31ca2c54fae7379bc6df38cc4fffaf04c34b94c
Duy Pham <duy211099@gmail.com>
2020-09-30 22:24:28 +0700

    Migrations

--- pyShop/products/models.py
+++ pyShop/products/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
+# py manage.py makemigrations
+# py manage.py migrate
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
     image_url = models.CharField(max_length=2083)
-    
+
 
