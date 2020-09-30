30f3b1bcdeaa4e60fe5c861a349c9e8f07973603
Duy Pham <duy211099@gmail.com>
2020-09-30 22:11:46 +0700

    Create products/new. Create Product Model

--- pyShop/products/views.py
+++ pyShop/products/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# URL - Uniform Resource Locator (Address)
+
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+
+def new(request):
+    return HttpResponse('New product')
