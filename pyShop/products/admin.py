0a45a391f0c51e67d576dddb36b01d732d199373
Duy Pham <duy211099@gmail.com>
2020-09-30 23:10:52 +0700

    admin Offer

--- pyShop/products/admin.py
+++ pyShop/products/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Offer
 # Register your models here.
 # py manage.py createsuperuser
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'stock', 'price')
 
 
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
